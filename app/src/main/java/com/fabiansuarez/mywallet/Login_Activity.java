9f3b2776cab4f7dc19916319c071f00917720538
mike <maicolflorez1305@gmail.com>
2023-10-13 15:12:37 -0500

    Avance Login/Register

--- app/src/main/java/com/fabiansuarez/mywallet/Login_Activity.java
+++ app/src/main/java/com/fabiansuarez/mywallet/Login_Activity.java
@@ -2,7 +2,10 @@ package com.fabiansuarez.mywallet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login_Activity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class Login_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button Login = findViewById(R.id.btn_register);
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login_Activity.this, Register_Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/fabiansuarez/mywallet/Register_Activity.java
+++ app/src/main/java/com/fabiansuarez/mywallet/Register_Activity.java
@@ -2,7 +2,10 @@ package com.fabiansuarez.mywallet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Register_Activity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class Register_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        ImageButton btnVolver = findViewById(R.id.ibt_volver);
+
+        btnVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent log = new Intent(Register_Activity.this,Login_Activity.class);
+                startActivity(log);
+            }
+        });
     }
 }
